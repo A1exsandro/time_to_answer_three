44791e6b89f1f4fe005e50fc8497a2bbdaad5241
A1exsandro <alexsandro.acp@gmail.com>
2022-04-19 11:58:30 -0400

    refactor: add modal window

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,8 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admins, skip: [:registrations]
 
+  get 'backoffice', to: 'admins_backoffice/welcome#index'
+
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
